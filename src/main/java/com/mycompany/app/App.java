8fd3f6dae2d429fe6766cada283839c89a8bdeb1
tushar35314 <tushar35314@gmail.com>
2019-06-08 18:42:55 +0530

    Seond change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! BOB";
+    private final String message = "Hello World! TUshar";
 
     public App() {}
 
